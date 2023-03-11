dda0bccb78d3047fdb0fd6fd46d586a84d6e9c6b
Luis Bazan <luis@bazan.pe>
2023-03-11 10:29:43 -0500

    First commit

---
+++ src/main/java/pe/bazan/luis/plugins/miprimerplugin/MiPrimerPlugin.java
@@ -0,0 +1,17 @@
+package pe.bazan.luis.plugins.miprimerplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MiPrimerPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
