97c4662ac94ee64d62b7d88755cc172e743d0385
Luis Bazan <luis@bazan.pe>
2023-03-16 10:32:01 -0500

    Create tabcompleter

--- src/main/java/pe/bazan/luis/plugins/miprimerplugin/commands/HelloCommand.java
+++ src/main/java/pe/bazan/luis/plugins/miprimerplugin/commands/HelloCommand.java
@@ -3,12 +3,41 @@ package pe.bazan.luis.plugins.miprimerplugin.commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 
-public class HelloCommand implements CommandExecutor {
+import javax.print.DocFlavor;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HelloCommand implements CommandExecutor, TabCompleter {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         // Logica del comando
         sender.sendMessage("Hello world from my command");
         return true;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
+        // Logic tabcomplete
+        List<String> completer = new ArrayList<>();
+
+        // /hello mu
+        // mundo
+        // mundito
+        // world
+
+        if (args.length == 1) {
+            String arg0 = args[0];
+
+            String[] words = new String[]{"world", "mundo", "mundito"};
+            for (String search : words) {
+                if (search.startsWith(arg0)) {
+                    completer.add(search);
+                }
+            }
+        }
+
+        return completer;
     }
 }
